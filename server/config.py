007e82574eb77851266d0011ee5c9d614870620c
selimbin <selim.elindary@student.guc.edu.eg>
2024-01-24 12:19:03 +0100

    replication_and_server_utils_5

--- server/config.py
+++ server/config.py
@@ -4,5 +4,4 @@ SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 10005
 MAX_RETRIES = 3
 RETRY_DELAY = 2
-
-# Add more server-specific configurations here
+HEARTBEAT_TIME = 10
--- server/server.py
+++ server/server.py
@@ -5,7 +5,7 @@ import threading
 import time
 import uuid
 
-from config import SERVER_HOST, SERVER_PORT, MAX_RETRIES, RETRY_DELAY
+from config import SERVER_HOST, SERVER_PORT, MAX_RETRIES, RETRY_DELAY, HEARTBEAT_TIME
 from file_storage.file_manager import FileManager
 from server_operations.server_utilities import handle_request
 from file_storage.replication_manager import ReplicationManager
@@ -22,22 +22,29 @@ class FileServer:
         self.host = SERVER_HOST
         self.max_retries = MAX_RETRIES
         self.retry_delay = RETRY_DELAY
+        self.heartbeat_time = HEARTBEAT_TIME
         self.port = 10005
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((self.host, self.port))
         self.file_manager = FileManager(self.server_name)
         self.servers = [('127.0.0.1', 10005), ('127.0.0.1', 10006)]
         self.is_leader = True
+
+        self.leader = ('127.0.0.1', 10005)
         self.replication_manager = ReplicationManager(self.is_leader, self.servers, self.file_manager)
-        self.request_history = []
+        # self.request_history = []
         self.queue = [('127.0.0.1', 10006)]
         self.hold_queue = []
+        self.history = {}
+        self.semaphores = {}
+        self.in_process = []
 
         self.raised = False
 
     def start(self):
         self.sock.listen()
         print(f"Server listening on {self.host}:{self.port}")
+        threading.Thread(target=self.heartbeat).start()
         while True:
             client, _ = self.sock.accept()
             if self.is_leader and len(self.servers) > 1 and len(self.queue) == 0:
@@ -48,55 +55,141 @@ class FileServer:
             if len(self.hold_queue) > 0 and len(self.queue) > 0:
                 threading.Thread(target=self.handle_incoming, args=(client, )).start()
 
+    def heartbeat(self):
+        start_time = time.perf_counter()
+        while True:
+            if self.is_leader:
+                if time.perf_counter() - start_time > 10:
+                    print(f"Performing Heartbeat on Children Servers")
+                    # perform heartbeat
+                    for address in self.queue:
+                        retry_count = 0
+                        response = None
+                        while retry_count <= self.max_retries and not response:
+                            print(f"in while: {retry_count}")
+                            try:
+                                new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                                new_socket.connect(address)
+                                new_socket.sendall("HEARTBEAT".encode())
+                                response = new_socket.recv(1024).decode()
+                            except Exception as e:
+                                pass
+                            if not response:
+                                print(f"No Heartbeat, Retry {retry_count + 1} for server {address}")
+                                time.sleep(self.retry_delay)
+                                retry_count += 1
+                        if not response:
+                            print(f"After {self.max_retries} HEARTBEATS, leader assumes server {address} is dead.")
+                            self.servers.remove(address)
+                            self.queue.remove(address)
+                            print(f"New child servers available are {self.servers}.")
+                    start_time = time.perf_counter()
+                    print("Completed Heartbeats on servers")
+
+            else:
+                if time.perf_counter() - start_time > 10:
+                    print(f"Performing Heartbeat on Leader")
+                    retry_count = 0
+                    response = None
+                    while retry_count <= self.max_retries and not response:
+                        try:
+                            new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            new_socket.connect(self.leader)
+                            new_socket.sendall("HEARTBEAT".encode())
+                            response = new_socket.recv(1024).decode()
+                        except Exception as e:
+                            pass
+                        if not response:
+                            print(f"No Heartbeat, Retry {retry_count + 1} for Leader {self.leader}")
+                            time.sleep(self.retry_delay)
+                            retry_count += 1
+                    if not response:
+                        print(f"After {self.max_retries} HEARTBEATS leader is assumed dead, Launching leader election.")
+                        self.servers.remove(self.leader)
+                    else:
+                        print("Completed heartbeat, Leader is alive")
+                        # Start leader election here
+                    start_time = time.perf_counter()
+
     def handle_incoming(self, client):
         try:
             data = client.recv(1024).decode()
         except Exception as e:
             print(f"Error while receiving data: {e}")
             return
-        completed = False
-        while not completed:
-            try:
-                if self.is_leader and len(self.servers) != 1 and len(self.queue) != 0:
-                    server_queue = self.queue.pop(0)
-                    # threading.Thread(target=self.client_handler, args=(client, server_queue)).start()
-                    self.client_handler(client, server_queue, data)
-                    self.queue.append(server_queue)
-                    completed = True
-                else:
-                    if self.is_leader and len(self.servers) > 1:
-                        self.hold_queue.append(client)
+        if data == "HEARTBEAT":
+            print(f"Received heartbeat request from server {client}")
+            client.sendall("ACKNOWLEDGING HEARTBEAT".encode())
+            return
+
+        unique_id, _, _, _ = parse_request(data)
+        if unique_id not in self.in_process:
+            completed = False
+            while not completed:
+                try:
+                    if self.is_leader and len(self.servers) != 1 and len(self.queue) != 0:
+                        server_queue = self.queue.pop(0)
+                        # threading.Thread(target=self.client_handler, args=(client, server_queue)).start()
+                        self.client_handler(client, server_queue, data)
+                        self.queue.append(server_queue)
                         completed = True
                     else:
-                        # threading.Thread(target=self.client_handler, args=(client, 'None')).start()
-                        self.client_handler(client, 'None', data)
-                        completed = True
-            except Exception as e:
-                print(e)
-                completed = False
+                        if self.is_leader and len(self.servers) > 1:
+                            self.hold_queue.append(client)
+                            completed = True
+                        else:
+                            # threading.Thread(target=self.client_handler, args=(client, 'None')).start()
+                            self.client_handler(client, 'None', data)
+                            completed = True
+                except Exception as e:
+                    print(e)
+                    completed = False
+        else:
+            if unique_id in self.history.keys():
+                try:
+                    client.sendall(self.history.get(unique_id).encode())
+                except Exception as e:
+                    print(f"Error {e} , Issue in sending response to client or server.")
+            else:
+                print(f"Request {unique_id} is still being processed.")#Should we include this in client implementation?
+
 
     def client_handler(self, client_socket, child_server, data):
         try:
             with client_socket:
                 unique_id, operation, file_name, file_data = parse_request(data)
                 print(f"Parse Results: {unique_id}, {operation}, {file_name}, {file_data}")
-                if unique_id in self.request_history:
-                    response = "operation already executed"
-                    client_socket.sendall(response.encode())
+                if unique_id in self.history.keys():
+                    response = self.history.get(unique_id)
+                    try:
+                        client_socket.sendall(response.encode())
+                    except Exception as e:
+                        print(f"Error {e} , Issue in sending response to client or server.")
                     return
+                self.in_process.append(unique_id)
+                if file_name not in self.semaphores.keys():
+                    self.semaphores.update({file_name: True})
+                if operation in ["WRITE", "EDIT", "DELETE", "CREATE"]:
+                    if self.semaphores.get(file_name):
+                        self.semaphores.update({file_name: False})
+                    else:
+                        raise Exception(f"Semaphore for file {file_name} is taken")
+
                 if self.is_leader and child_server != 'None':
                     print(f"Sending request from leader to server: {str(child_server)}")
                     retry_count = 0
                     response = None
                     while retry_count <= self.max_retries and not response:
-                        new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        new_socket.connect(child_server)
-                        new_socket.sendall(data.encode())
-                        # Expecting an acknowledgment from backup
-                        time.sleep(self.retry_delay)
-                        response = new_socket.recv(1024).decode()
+                        try:
+                            new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            new_socket.connect(child_server)
+                            new_socket.sendall(data.encode())
+                            response = new_socket.recv(1024).decode()
+                        except Exception as e:
+                            pass
                         if not response:
-                            print(f"Retry {retry_count + 1} failed for server {child_server}")
+                            print(f"No response, Retry {retry_count + 1} for server {child_server}")
+                            time.sleep(self.retry_delay)
                             retry_count += 1
                     if not response:
                         print(f"After {self.max_retries} retries, leader assumes server {child_server} is dead.")
@@ -121,12 +214,14 @@ class FileServer:
                         # socket_replication = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                         if "successfully" in response:
                             response = unique_id + " REPLICATE " + file_name + " " + file_data
+
+                self.history.update({unique_id: response})
                 try:
                     client_socket.sendall(response.encode())
                 except Exception as e:
                     print(f"Error {e} , Issue in sending response to client or server.")
 
-                self.request_history.append(unique_id)
+                # self.request_history.append(unique_id)
                 if child_server != 'None':
                     self.queue.append(child_server)
         except Exception as e:
